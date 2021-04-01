e05f67f99a890b87d9c5fb8af0b5237cb637908b
Abdullah Ismail <ohida2001@gmail.com>
2021-04-01 16:47:39 +0100

    Add product repo

---
+++ src/main/java/EcommerceApp/Spring/boot/Project/model/Cart.java
@@ -0,0 +1,15 @@
+package EcommerceApp.Spring.boot.Project.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Cart {
+
+    String name;
+    String number;
+    String p;
+
+}
--- src/main/java/EcommerceApp/Spring/boot/Project/model/Product.java
+++ src/main/java/EcommerceApp/Spring/boot/Project/model/Product.java
@@ -1,4 +1,10 @@
 package EcommerceApp.Spring.boot.Project.model;
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "product")
+
 public class Product {
+
 }
